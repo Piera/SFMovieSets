b1081071745cf78fc0e678d5115d649942bd9fa7
Piera <pierad@gmail.com>
2014-12-30 19:30:37 -0800

    Updated model & add script - now relational for movies & locations

--- add_movies.py
+++ add_movies.py
@@ -2,29 +2,70 @@
 import json
 import urllib2
 import model
-from model import Movie_Location
+from model import Movie, Movie_Location
+from model import session as dbsession
 from datetime import datetime
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 
-# Fill the database of with movie location data
-def load_movie_location_data(session):
+# Fill the database of with movie overview and movie location data
+def load_movie_data(dbsession):
 	movie_data = None
-	movie_url="https://data.sfgov.org/api/views/yitu-d5am/rows.json?accessType=DOWNLOAD"
+	movie_url = "https://data.sfgov.org/api/views/yitu-d5am/rows.json?accessType=DOWNLOAD"
 	req = urllib2.Request(movie_url)
 	response = urllib2.urlopen(req)
 	movie_data = json.load(response)
+	print movie_data
 	for movie in movie_data['data']:
-		movie_location = model.Movie_Location(\
-							movie_title = movie[8],\
-							year = (movie[9]),\
-							location = movie[10]\
-							)
-		session.add(movie_location)
-	session.commit()
+		title = movie[8]
+		print title
+		movie_in_db = dbsession.query(model.Movie).filter_by(movie_title = title).all()
+		print movie_in_db
+		if movie_in_db == []:
+			year = movie[9]
+			print "YEAR", year
+			movies = model.Movie(\
+							movie_title = title,\
+							year = year)
+			dbsession.add(movies)
+			dbsession.commit()
+		else:
+			pass
+
+def load_movie_location_data(session):
+	movie_locations = None
+	fun_fact = None
+	movie_url="https://data.sfgov.org/api/views/yitu-d5am/rows.json?accessType=DOWNLOAD"
+	req = urllib2.Request(movie_url)
+	response = urllib2.urlopen(req)
+	movie_locations = json.load(response)
+	for movie in movie_locations['data']:
+		title = movie[8]
+		location = movie[10]
+		print "Now second function"
+		print "title", title, "location", location
+		movie_id=dbsession.query(model.Movie).filter_by(movie_title = title).all()
+		print movie_id[0].id
+		location_in_db = dbsession.query(model.Movie_Location).filter_by(location = location).filter_by(movie_id = movie_id[0].id).all()
+		print location_in_db
+		if location_in_db == []:
+			if movie[11]:
+				fun_fact = movie[11]
+			else:
+				fun_fact = None
+				print "FUN FACT", fun_fact
+			movie_locations = model.Movie_Location(\
+							movie_id = movie_id[0].id,\
+							location = location,\
+							fun_facts = fun_fact)
+			dbsession.add(movie_locations)
+		else:
+			pass 
+	dbsession.commit()
 
 def main():
-	model.create_table()
+	model.create_tables()
 	s = model.add_data()
+	load_movie_data(s)
 	load_movie_location_data(s)
     
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -1,42 +1,45 @@
 import os
 import psycopg2
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker, scoped_session, relationship, backref
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 
-# ENGINE = create_engine('postgresql:///pieradamonte', echo = True)
-# DATABASE_URL = 'postgresql:///pieradamonte'
 DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql:///pieradamonte')
 ENGINE = create_engine(DATABASE_URL, echo = False)
 
 session = scoped_session(sessionmaker(bind=ENGINE, autocommit = False, autoflush = False)) 
 Base = declarative_base()
 
-class Movie_Location(Base):
-    __tablename__ = "movie_locations"
+class Movie(Base):
+    __tablename__ = "movies"
 
     id = Column(Integer, primary_key = True)
     movie_title = Column(String(200), nullable = False)
     year = Column(Integer, nullable=False)
+
+class Movie_Location(Base):
+    __tablename__ = "movie_locations"
+
+    id = Column(Integer, primary_key = True)
+    movie_id = Column(Integer, ForeignKey('movies.id'))
     location = Column(String(200), nullable = True)
-  
+    fun_facts = Column(String(500), nullable = True)
+    movie = relationship("Movie", backref=backref("movie_locations", order_by=id))
 
 def add_data():
     global ENGINE
     global Session
 
-    # ENGINE = create_engine('postgresql:///pieradamonte', echo = True)
     ENGINE = create_engine(DATABASE_URL, echo = False)
     Session = scoped_session(sessionmaker(bind=ENGINE, autocommit = False, autoflush = False)) 
 
     session = Session()
     return Session()
 
-def create_table():
+def create_tables():
     global ENGINE
     global Session
 
-    # ENGINE = create_engine('postgresql:///pieradamonte', echo = True)
     ENGINE = create_engine(DATABASE_URL, echo = False)
     Session = sessionmaker(bind=ENGINE)
     Base.metadata.create_all(ENGINE)
@@ -44,7 +47,6 @@ def create_table():
     return Session()
 
 def main():
-    """Placeholder"""
     pass
 
 if __name__ == "__main__":
